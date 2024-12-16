ea3ffdb87af70cb73ac8ebc1570a5b2f609b8092
Raissa Nv <112831564+RaissaNv@users.noreply.github.com>
2024-12-16 16:09:38 -0300

    exercicio de condicao simples

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var vel = 12
+ console.log(`A velocidade do seu carro e ${vel}Km/h`)
+
+ if (vel > 60) { // Condicao simples
+    console.log('Voce ultrapassou o limite de velocidade. MULTADO.')
+}
+
+ console.log('Dirija sempre usando cinto de seguranca.')
